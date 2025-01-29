f25da7e5d47dc25a325e68875bca1a4d20e24555
Malmilanit <lanitmalmi@gmail.com>
2025-01-29 12:44:16 +0200

    Update App.tsx & update pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,11 @@ const About = () => {
   return (
     <div className="text-white text-center p-8">
       <h1 className="text-4xl font-bold">Tietoa tapahtumasta</h1>
-      <p>Malmin LANIT 2025 on pelitapahtuma kaikille kiinnostuneille.</p>
+      <p>Malmin sydämessä, olemme yhdistäneet pelit, visiot, alkoholin ja hauskanpidon – synnyttäen unelmien LAN-kokemuksia, joita ei ole ennen nähty. 
+	  Täällä, missä kaverukset, tietokoneet ja nörttiys kohtaavat, syntyy tapahtumia, jotka jäävät historiaan. 
+	  Me emme vain pelaa – me luomme legendoja, yksi strategia ja ottelu kerrallaan. CS2:n taktiikoista OW2:n tiimiyhteistyöhön, 
+	  Deadlockin intensiivisiin taisteluihin ja ProPilkki-taituruuteen, me haastamme toisiamme ja valloittamme pelimaailman yhdessä. 
+	  Valmistaudu sukeltamaan Malmin sykkeeseen, missä virtaava olut ja eeppiset pelit luovat täydellisen alustan maailman parhaalle turnaukselle!</p>
     </div>
   );
 };
