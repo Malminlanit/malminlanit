108766445c266b02a6cb4829001b96068e34a4fc
Malmilanit <lanitmalmi@gmail.com>
2025-01-29 12:21:56 +0200

    Update App.tsx & update pages

--- src/App.tsx
+++ src/App.tsx
@@ -130,6 +130,9 @@ function App() {
             <h3 className="text-xl font-bold mb-2">Ruoka & juoma</h3>
             <p>24/7 Snack Bar, omakustanteinen</p>
             <p>Lähellä Alepa</p>
+			<div className="w-full h-96 rounded-lg overflow-hidden shadow-lg">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15836.769048179707!2d24.983312487602227!3d60.25359860310647!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469209b353fea185%3A0x22cfbb739db1864f!2sAlepa%20Teerisuontie!5e0!3m2!1sfi!2sfi!4v1738146067686!5m2!1sfi!2sfi" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
           </div>
         </div>
       </div>
