7d8c1a9a9179ab7ee1076742d91c9d384bdfec60
Malmilanit <lanitmalmi@gmail.com>
2025-02-03 01:36:45 +0200

    Päivitetty aikataulu

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Määrittele Supabase URL ja API-avaimet, jotka löydät Supabase-konsolista
 const supabase = createClient(
-  'https://ugbtawotpwglwoujauzs.supabase.co',  // Supabase-projektisi URL
+  'https://ugbtawotpwglwoujauzs.supabase.com',  // Supabase-projektisi URL
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnYnRhd290cHdnbHdvdWphdXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1MzYwNjIsImV4cCI6MjA1NDExMjA2Mn0.9j-Egvs9bJHzsmUDmQ1WVwm9EjtkOvbbS1E6wmEFp1M'  // Supabase-projektisi anonyymi avain
 );
 
