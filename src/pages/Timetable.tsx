396e2df23ed5fdcb525e15f9a8ac7dc589a53488
Malmilanit <lanitmalmi@gmail.com>
2025-02-03 01:31:30 +0200

    Päivitetty aikataulu

--- src/pages/Timetable.tsx
+++ src/pages/Timetable.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import supabase from '../services/supabase';  // Tuodaan supabase.js
+import supabase from '../services/supabase';
 
 function Schedule() {
   const [scheduleData, setScheduleData] = useState(null);
@@ -8,41 +8,38 @@ function Schedule() {
   useEffect(() => {
     const getSchedule = async () => {
       try {
-        // Kysely Supabaseen
         const { data, error } = await supabase
-          .from('schedules')  // Taulu, johon haetaan dataa
-          .select('*');  // Haetaan kaikki kentät
+          .from('schedules')
+          .select('*');
 
         if (error) {
-          throw error;  // Jos virhe, heitetään virhe
+          throw error;
         }
 
-        // Jos data löytyy, tallennetaan se tilaan
         setScheduleData(data);
       } catch (error) {
-        // Jos kyselyssä tapahtui virhe, asetetaan virhetilanne
         setError(error.message);
         console.error('Virhe Supabase-yhteydessä:', error);
       }
     };
 
-    getSchedule();  // Suoritetaan kysely heti, kun komponentti ladataan
-  }, []);  // Tyhjä riippuvuuslista, joten kysely ajetaan vain kerran
+    getSchedule();
+  }, []);
 
   return (
     <div>
       <h1>Aikataulu</h1>
 
-      {error && <p className="text-red-500">{`Virhe: ${error}`}</p>}  {/* Virhetilanne */}
+      {error && <p className="text-red-500">{`Virhe: ${error}`}</p>}
 
       {scheduleData ? (
         <div>
           <h2>Haettu Aikataulu</h2>
-          <pre>{JSON.stringify(scheduleData, null, 2)}</pre>  {/* Näytetään haettu data */}
+          <pre>{JSON.stringify(scheduleData, null, 2)}</pre>
         </div>
       ) : (
-        <p>Ladataan aikataulua...</p> ) {/* Näytetään latausviesti */}
-      }
+        <p>Ladataan aikataulua...</p>
+      )}
     </div>
   );
 }
