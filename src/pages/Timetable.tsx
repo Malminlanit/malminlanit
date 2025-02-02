1fe2a1d2d62c40f86b1f8eedbc08c2ab9957d50f
Malmilanit <lanitmalmi@gmail.com>
2025-02-02 21:12:48 +0200

    Muokattu aikataulusivun kirjautuminen

--- src/pages/Timetable.tsx
+++ src/pages/Timetable.tsx
@@ -46,21 +46,23 @@ function Schedule() {
 
   const saveScheduleToGitHub = async (updatedData) => {
     const fileContent = JSON.stringify(updatedData, null, 2); // Muunna aikataulu JSON-muotoon
-    const base64Content = btoa(fileContent); // Muunna JSON base64-muotoon, jotta GitHub voi käsitellä sen
+    const base64Content = window.btoa(unescape(encodeURIComponent(fileContent))); // Muunna JSON base64-muotoon
+
+    const sha = await getFileSha();
 
     try {
       const response = await fetch(
-        `https://api.github.com/repos/Malminlanit/malminlanit/contents/public/schelude.json`,
+        `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`,
         {
           method: 'PUT',
           headers: {
-            'Authorization': `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+            'Authorization': `Bearer ${GITHUB_TOKEN}`,
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
             message: 'Päivitetään aikataulu', // Viesti GitHubin commitille
             content: base64Content, // Tiedoston sisältö base64-muodossa
-            sha: await getFileSha() // Tarvitaan tiedoston SHA-1, jos tiedosto on olemassa
+            sha: sha || undefined, // Tarvitaan tiedoston SHA-1, jos tiedosto on olemassa
           })
         }
       );
@@ -77,19 +79,28 @@ function Schedule() {
   };
 
   const getFileSha = async () => {
-    const response = await fetch(
-      `https://api.github.com/repos/Malminlanit/malminlanit/contents/public/schelude.json`,
-      {
-        headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    try {
+      const response = await fetch(
+        `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`,
+        {
+          headers: {
+            'Authorization': `Bearer ${GITHUB_TOKEN}`,
+          }
         }
+      );
+      const data = await response.json();
+
+      if (data.message === "Not Found") {
+        return null; // Return null if the file doesn't exist
       }
-    );
-    const data = await response.json();
-    return data.sha; // Tiedoston SHA-1
+
+      return data.sha; // Tiedoston SHA-1
+    } catch (error) {
+      console.error('Virhe tiedoston SHA:n hakemisessa:', error);
+      return null;
+    }
   };
 
-  // Lisätään tämä funktio, joka hyväksyy muutokset
   const handleSave = () => {
     alert('Muutokset on tallennettu!');
     setIsEditing(false); // Lopetetaan muokkaustila
