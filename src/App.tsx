3d11ee7ec37b7441701eb9a1ea893e83690bce8b
Malmilanit <lanitmalmi@gmail.com>
2025-02-02 18:23:17 +0200

    Päivitetty sivulinkkaus

--- src/App.tsx
+++ src/App.tsx
@@ -128,7 +128,7 @@ function App() {
           >
             Lue lisää
           </button>
-          <div className="absolute top-8 [calc(100%+16px)]left-1/2 -translate-x-1/2 animate-bounce">
+          <div className="absolute top- [calc(100%+16px)]left-1/2 -translate-x-1/2 animate-bounce">
             <ChevronDown className="w-8 h-8 text-white" />
           </div>
         </div>
