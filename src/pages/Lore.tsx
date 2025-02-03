7a3b8cd6c453cb89df65ae7eb70dc8878684761f
Malmilanit <lanitmalmi@gmail.com>
2025-02-03 19:47:28 +0200

    Päivitetty turnaussivusto

--- src/pages/Lore.tsx
+++ src/pages/Lore.tsx
@@ -44,7 +44,6 @@ const Lore = () => {
 					"Kauan sitten, Malmin mailla, siellä missä kadut mutkittelevat kuin ikivanha joki ja talot kohoavat korkealle kohti taivasta, syntyi suuri tarina.\n\n" +
 					"Malmin lännen alue oli tunnettu paitsi vilkkaista kauppakeskuksistaan ja harvinaisista taikatuotteistaan myös sen mystisestä tunnelmasta, joka veti seikkailijoita kaikkialta maailmasta.\n\n" +
 					"Kuningatar Taimi, Malmin suvereeni valtias, oli viisas ja hyväsydäminen.\n\n" +
-					<br />
 					"Hänen valtakuntansa oli täynnä salaisuuksia, ja hänen hallitsemansa Malmi oli kuin elävä organismi, joka hengitti katujen ja kujien välistä henkeä. Kuningatar Taimi oli kuitenkin pidetty enemmän kuin pelätty. Hän hallitsi Malmin kulmakaupunkia lempeällä kädellä, mutta tiesi, että salaisuudet saattavat kääntyä koko valtakunnan kohtaloksi.\n\n" +
 					"Malmin katujen välinen kulkureitti, tunnettu nimellä Kuun polku, oli vanha salainen tie, joka kuljetti asukkaita vuosituhansia taaksepäin.\n\n" +
 					"Tälle tielle oli piilotettu kadonneita aarteita ja kadonnut voima, mutta vain harvat olivat uskaltaneet astua sen varjoon. Moni oli kadonnut matkoillaan, mutta vain harvat palasivat kertomaan mitä he olivat nähneet.\n\n" +
@@ -53,13 +52,11 @@ const Lore = () => {
 					"Väinö ei ollut matkalla yksin.\n\n" +
 					"Hänen mukaansa liittyi Alma, Malmin tunnettu kirjanoppinut, joka oli itsekin salaa tutkiskellut Kuun polun arvoituksia. Alma oli se, joka tiesi kaiken Kuun polusta ja sen suojatuista alueista. Hänen älykkyytensä ja muistiinsanojen avulla he selvisivät monista ansoista, joita Varjokuningas oli asettanut suojelemaan itseään.\n\n" +
 					"Matka oli täynnä vaaroja.\n\n" +
-					<br />
 					"Eräällä käänteellä he kohtasivat Iivari, Malmin katujen kuuluisa varas, joka oli jäänyt juonensa vangiksi. Iivari oli kyllästynyt elämässään ja oli tullut etsimään Kuun polulta vapautusta, mutta ei ollut valmis maksamaan hintaa. Yhteinen taistelu ansoja vastaan pakotti kuitenkin Iivarin kääntämään kelkkansa ja liittymään Väinön ja Alman seuraan.\n\n" +
 					"Heidän matkaansa seurasi koko Malmin kaupunki.\n\n" +
 					"Kaupunkilaiset tiesivät, että jotakin suurta oli tapahtumassa, mutta Malmin kuningatar Taimi oli päättänyt antaa seikkailijoille vapauden päättää polkunsa. \"Heidän matkansa tulee olemaan heidän oma\", Kuningatar sanoi, mutta hän seurasi tarkasti heidän kulkuaan, sillä tämä seikkailu ei ollut pelkästään heidän oma.\n\n" +
 					"Matka Kuun polulle oli täynnä lumouksia, salaisuuksia ja maagisia portteja, mutta myös ystävyyksiä ja oivalluksia.\n\n" +
 					"Lopulta he saapuivat polun pimeimpään syvyyteen, missä he kohtasivat Varjokuninkaan — vanhan olennon, joka oli kahlittu ja jättänyt jälkensä kaikkeen Malmin varjoon. Se ei ollut pelkästään peto, vaan ikivanha voima, joka oli kerran hallinnut koko alueen.\n\n" +
-					<br />
 					"Taistelu oli kauhea.\n\n" +
 					"Mutta Väinön rohkeus, Alman älykkyys ja Iivarin ketteryys yhdistyivät ja he pystyivät voittamaan Varjokuninkaan. Kuningas jäi ikuisesti Malmin varjoihin, mutta he eivät voineet tuoda mukanaan sitä kaikkea, mitä polku oli jättänyt taakseen.\n\n" +
 					"Seikkailijat palasivat takaisin Malmiin, mutta heidän matkastaan tuli legenda.\n\n" +
