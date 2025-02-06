34c97607d3e4b834209dfa6a432e53d957ac5b0e
Malmilanit <lanitmalmi@gmail.com>
2025-02-06 17:55:58 +0200

    Lisätty game.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),  // Lisää alias src-hakemistoll
+      '@': path.resolve(__dirname, 'src'),  // Lisää alias src-hakemistolle
     },
   },
   base: '/malminlanit/',  // Muokkaa tämä, jos käytät GitHub Pagesia tai muuta isännöintipalvelua
